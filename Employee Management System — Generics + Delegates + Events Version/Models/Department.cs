bf0f48d1e7dcd7b13f711465e351afa918c4ac87
Ahmed Saeed <Ahmed Saeed@SHS5CD9504LW8C>
2026-08-18 08:25:43 +0300

    add Employee , Depaertment , Manager Models

---
+++ Employee Management System — Generics + Delegates + Events Version/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System___Generics___Delegates___Events_Version.Models
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ Employee Management System — Generics + Delegates + Events Version/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System___Generics___Delegates___Events_Version.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+        public int DepartmentId { get; set; }
+
+        public override string ToString()
+        {
+            return $"-Name : {Name}\n-Id : {Id}\n-DepartmentId : {DepartmentId}\n-Salary : {Salary}\n-HireDate : {HireDate}";
+        }
+    }
+}
