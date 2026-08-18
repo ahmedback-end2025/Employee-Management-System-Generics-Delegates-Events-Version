9a7868a75d224849260dce14b078fdd85a1859a0
Ahmed Saeed <Ahmed Saeed@SHS5CD9504LW8C>
2026-08-18 11:37:24 +0300

    add company collections

---
+++ Employee Management System — Generics + Delegates + Events Version/Services/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System___Generics___Delegates___Events_Version.Services
+{
+    internal class Company
+    {
+
+    }
+}
