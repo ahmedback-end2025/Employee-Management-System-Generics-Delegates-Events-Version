be8f4d3961e9e3715d587b0c8b42172f53fd5b5d
Ahmed Saeed <Ahmed Saeed@SHS5CD9504LW8C>
2026-08-18 09:10:58 +0300

    add generic result type

---
+++ Employee Management System — Generics + Delegates + Events Version/Common/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System___Generics___Delegates___Events_Version.Common
+{
+    internal class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public string Message { get; }
+
+        public T data { get; }
+
+        public Result(bool _IsSuccess , string message ,T _data)
+        {
+            IsSuccess = _IsSuccess;
+            Message = message;
+            data = _data;
+        }
+
+
+        public static Result<T> Success(T obj, string message = "Success")
+        {
+            return new Result<T>(true, message,obj);
+        }
+
+        public static Result<T> Failure(string message = "Failure")
+        {
+            return new Result<T>(false, message, default);
+        }
+    }
+}
