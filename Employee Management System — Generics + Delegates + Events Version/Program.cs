f1e21e97610c7e6552ad09eacdd53f2619d79e6a
Ahmed Saeed <Ahmed Saeed@SHS5CD9504LW8C>
2026-08-18 08:16:35 +0300

    Add project files.

---
+++ Employee Management System — Generics + Delegates + Events Version/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee_Management_System___Generics___Delegates___Events_Version
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
